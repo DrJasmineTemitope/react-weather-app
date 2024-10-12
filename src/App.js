db8244d433b4add6cc3fb0b7922b0ac6ceba7514
DrJasmineTemitope <120563093+DrJasmineTemitope@users.noreply.github.com>
2024-10-12 21:47:20 +0100

    Added a link to open source of the code on github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <h1>REACT WEATHER APP</h1>
 
       <Weather />
+      <p>
+        {" "}
+        coded by Temitope with React{" "}
+        <a
+          href="https://github.com/DrJasmineTemitope/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Opensource
+        </a>
+      </p>
     </div>
   );
 }
